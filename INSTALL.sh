63c13819f3e9dc9a655d5562b5315abdf1268ed5
tombstoneGhost <simardeepsngh1@gmail.com>
2021-01-17 15:07:55 +0530

    Installation Script

---
+++ INSTALL.sh
@@ -0,0 +1,17 @@
+# Copying Custom Modules
+sudo mkdir /usr/local/go/src/CXplorer
+sudo cp -r modules/ /usr/local/go/src/CXplorer
+
+# Installing Required C Libraries
+sudo apt-get install libpcap-dev
+
+# Installing Required Go Packages
+go get github.com/google/gopacket
+go get golang.org/x/net/bpf
+go get golang.org/x/sys/unix
+
+# Building Tool
+go build -o CXplorer
+
+# Moving Executable to Bin
+sudo cp CXplorer /usr/local/bin/
